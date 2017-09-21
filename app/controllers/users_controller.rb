6236c9d735b95513bf5ebe195b0c5cc789ef67e2
tim-lee92 <tim-lee92@hotmail.com>
2017-09-20 17:15:17 -0700

    Fix null fields for shouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < Clearance::UsersController
   def new
     @user = User.new
-    render template: "users/new"
   end
 
   def show
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,7 +1,7 @@
 class Shout < ActiveRecord::Base
   belongs_to :user
   belongs_to :content, polymorphic: true
-
+  
   validates :user, presence: true
 
   default_scope { order(created_at: :desc) }
