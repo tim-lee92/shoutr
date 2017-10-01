a4963af9d7a98ba505f1f91a016b0008f387b4d9
tim-lee92 <tim-lee92@hotmail.com>
2017-10-01 14:04:21 -0700

    Add autolink functionality

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -13,4 +13,10 @@ module ShoutHelper
       link_to 'Like', like_shout_path(shout), method: :post
     end
   end
+
+  def autolink(text)
+    text.gsub(/@\w+/) do |mention|
+      link_to mention, user_path(mention[1..-1])
+    end.html_safe
+  end
 end
