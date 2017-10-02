3d90d184b0493c7bcaeb9c20ff7e8132c5db922b
tim-lee92 <tim-lee92@hotmail.com>
2017-10-01 18:35:01 -0700

    Refactor shout forms

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -12,4 +12,12 @@ module ShoutHelper
       link_to mention, user_path(mention[1..-1])
     end.html_safe
   end
+
+  def shout_form_for(content_type)
+    form_for Shout.new do |form|
+      form.hidden_field(:content_type, value: content_type) +
+      form.fields_for(:content) { |content_form| yield(content_form) } +
+      form.submit('Shout!')
+    end
+  end
 end
